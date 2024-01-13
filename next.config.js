4d5b2b3d1becb7da39ae9a08978e54a82d25bb2a
bKoZii <bigkonkamon@gmail.com>
2024-01-13 09:57:23 +0700

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -1,50 +1,39 @@
+const { withPayload } = require("@payloadcms/next-payload");
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const ContentSecurityPolicy = require('./csp')
-const redirects = require('./redirects')
-
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
-      .filter(Boolean)
-      .map(url => url.replace(/https?:\/\//, '')),
-  },
-  redirects,
-  async headers() {
-    const headers = []
-
-    // Prevent search engines from indexing the site if it is not live
-    // This is useful for staging environments before they are ready to go live
-    // To allow robots to crawl the site, use the `NEXT_PUBLIC_IS_LIVE` env variable
-    // You may want to also use this variable to conditionally render any tracking scripts
-    if (!process.env.NEXT_PUBLIC_IS_LIVE) {
-      headers.push({
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'noindex',
-          },
-        ],
-        source: '/:path*',
-      })
-    }
-
-    // Set the `Content-Security-Policy` header as a security measure to prevent XSS attacks
-    // It works by explicitly whitelisting trusted sources of content for your website
-    // This will block all inline scripts and styles except for those that are allowed
-    headers.push({
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: ContentSecurityPolicy,
-        },
+const nextConfig = withPayload(
+  {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    transpilePackages: [
+      "@payloadcms/plugin-seo",
+      "payload/components/forms",
+      "payload/components",
+    ],
+    images: {
+      domains: [
+        "localhost",
+        "nextjs-vercel.payloadcms.com",
+        process.env.NEXT_PUBLIC_APP_URL,
+        `${process.env.NEXT_PUBLIC_S3_ENDPOINT}`.replace("https://", ""),
       ],
-    })
-
-    return headers
+    },
+    webpack: {
+      resolve: {
+        alias: {},
+      },
+    },
   },
-}
+  {
+    configPath: path.resolve(__dirname, "./payload/payload.config"),
+  }
+);
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: true,
+});
+
+module.exports =
+  process.env.ANALYZE === "true" ? withBundleAnalyzer(nextConfig) : nextConfig;
